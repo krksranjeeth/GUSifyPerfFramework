623de76e78e3695043ee9adef85240f01c52cc32
Ranjeeth <rkathiresan@salesforce.com>
2018-11-21 18:26:52 -0800

    First commit to check Heroku

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Welcome to GUSify Perf Framework");
+
+const express = require("express");
+const app = express();
+
+let port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`Listening on the port ${port}`));
+
+app.get("/", function(request, response) {
+    response.send("Hello");
+})
